31d14b25adb4da40baa19434e64a04c69c320493
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-10 03:39:53 +0100

    22222

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,26 @@
-let myTimeout;
+let timeoutVariable;
+//let counteColorChange = 0;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = getRandomHexColor();
-    myTimeout = setTimeout(changeBackgroundColor, 1000);
+  document.body.style.backgroundColor = getRandomHexColor();
+  timeoutVariable = setTimeout(changeBackgroundColor, 1000);
+  //counteColorChange += 1;
+  //console.log(counteColorChange);
 }
 
-document.querySelector("[data-start]").addEventListener("click", () => {
-    document.querySelector("[data-start]").disabled = true;
-    document.querySelector("[data-stop]").disabled = false;
-    changeBackgroundColor()
-  });
+document.querySelector('[data-start]').addEventListener('click', () => {
+  document.querySelector('[data-start]').disabled = true;
+  document.querySelector('[data-stop]').disabled = false;
+  changeBackgroundColor();
+});
 
-document.querySelector("[data-stop]").addEventListener("click", () => {
-    document.querySelector("[data-stop]").disabled = true;
-    document.querySelector("[data-start]").disabled = false;
-    clearTimeout(myTimeout);
-  });
+document.querySelector('[data-stop]').addEventListener('click', () => {
+  document.querySelector('[data-stop]').disabled = true;
+  document.querySelector('[data-start]').disabled = false;
+  clearTimeout(timeoutVariable);
+  //counteColorChange = 0;
+});
