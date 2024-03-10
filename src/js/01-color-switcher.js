897fb17b8a3197aa8f2bcf04ce16881511c83952
AgnieszkaWIT <151778905+AgnieszkaWIT@users.noreply.github.com>
2024-03-10 02:38:38 +0100

    111

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-console.log("Test log");
+let myTimeout;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    myTimeout = setTimeout(changeBackgroundColor, 1000);
+}
+
+document.querySelector("[data-start]").addEventListener("click", () => {
+    document.querySelector("[data-start]").disabled = true;
+    document.querySelector("[data-stop]").disabled = false;
+    changeBackgroundColor()
+  });
+
+document.querySelector("[data-stop]").addEventListener("click", () => {
+    document.querySelector("[data-stop]").disabled = true;
+    document.querySelector("[data-start]").disabled = false;
+    clearTimeout(myTimeout);
+  });
